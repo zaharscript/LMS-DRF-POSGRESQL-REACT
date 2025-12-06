26fe354e4acc900741f007999adf8ce643fb9428
Zahar <87757637+zaharscript@users.noreply.github.com>
2025-12-06 22:01:26 +0800

    Implement custom hook for theme management

---
+++ frontend/src/hook/useTheme.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export default function useTheme() {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "light"
+  );
+
+  useEffect(() => {
+    const html = document.documentElement;
+
+    if (theme === "dark") {
+      html.classList.add("dark");
+    } else {
+      html.classList.remove("dark");
+    }
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+
+  return { theme, toggleTheme };
+}
