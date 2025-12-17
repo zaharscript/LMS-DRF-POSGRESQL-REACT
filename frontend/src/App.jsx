b34b020400a5b623bb7833335ac7a8c09575affe
unknown <zahratulnazirah@gmail.com>
2025-12-17 22:13:10 +0800

    remove mobile dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
 import MainLayout from "./layout/MainLayout";
 import DashboardPage from "./pages/DashboardPage";
-import MobileDashboard from "./pages/MobileDashboard";
 import CourseDetails from "./pages/CourseDetails";
 import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-
-  useEffect(() => {
-    const onResize = () => setIsMobile(window.innerWidth < 768);
-    window.addEventListener("resize", onResize);
-    return () => window.removeEventListener("resize", onResize);
-  }, []);
-
   return (
     <ThemeProvider>
       <Router>
-        <Routes>
-          {/* DASHBOARD */}
-          <Route
-            path="/"
-            element={
-              isMobile ? (
-                <MobileDashboard />
-              ) : (
-                <MainLayout>
-                  <DashboardPage />
-                </MainLayout>
-              )
-            }
-          />
-
-          {/* COURSE DETAILS (shared layout) */}
-          <Route
-            path="/course/:id"
-            element={
-              <MainLayout>
-                <CourseDetails />
-              </MainLayout>
-            }
-          />
-        </Routes>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/course/:id" element={<CourseDetails />} />
+          </Routes>
+        </MainLayout>
       </Router>
     </ThemeProvider>
   );
