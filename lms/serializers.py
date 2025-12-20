6e2b3007cf6bf6dbe66c2944fd948d0ea94aa7b9
Zahar <87757637+zaharscript@users.noreply.github.com>
2025-12-20 12:43:37 +0800

    Update serializers.py

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Course, Section, Topic
 
 
@@ -22,3 +23,18 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ['id', 'title', 'instructor','provider', 'date_joined', 'sections']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password")
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email", ""),
+            password=validated_data["password"],
+        )
+        return user
