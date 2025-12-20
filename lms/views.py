1ef482ec9dc5988729b94c38acd072018b2fe7e0
Zahar <87757637+zaharscript@users.noreply.github.com>
2025-12-20 12:52:21 +0800

    Update views.py

--- lms/views.py
+++ lms/views.py
@@ -1,31 +1,41 @@
 from rest_framework import generics, permissions
-from django.contrib.auth.models import User
-from .models import Course, Section, Topic
-from .serializers import RegisterSerializer, CourseSerializer, SectionSerializer, TopicSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 
+from .models import Course, Section, Topic
+from .serializers import (
+    RegisterSerializer,
+    CourseSerializer,
+    SectionSerializer,
+    TopicSerializer,
+)
 
+# -------------------------
+# AUTH
+# -------------------------
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def me(request):
     user = request.user
     return Response({
-        'id': user.id,
-        'username': user.username,
-        'email': user.email,
+        "id": user.id,
+        "username": user.username,
+        "email": user.email,
     })
 
-# ---- ACCOUNT REGISTRATION----
+
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
 
 
-# ---- COURSES ----
+# -------------------------
+# COURSES
+# -------------------------
 
 class CourseListCreateView(generics.ListCreateAPIView):
     serializer_class = CourseSerializer
@@ -37,8 +47,6 @@ class CourseListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-
-from rest_framework.permissions import IsAuthenticated
 
 class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CourseSerializer
@@ -48,31 +56,47 @@ class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
         return Course.objects.filter(user=self.request.user)
 
 
+# -------------------------
+# SECTIONS
+# -------------------------
 
-# ---- SECTIONS ----
-class CourseListCreateView(generics.ListCreateAPIView):
-    serializer_class = CourseSerializer
+class SectionListCreateView(generics.ListCreateAPIView):
+    serializer_class = SectionSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Course.objects.filter(user=self.request.user)
+        return Section.objects.filter(course__user=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
+        serializer.save()
 
 
 class SectionDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Section.objects.all()
     serializer_class = SectionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Section.objects.filter(course__user=self.request.user)
 
 
-# ---- TOPICS ----
+# -------------------------
+# TOPICS
+# -------------------------
+
 class TopicListCreateView(generics.ListCreateAPIView):
-    queryset = Topic.objects.all()
     serializer_class = TopicSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Topic.objects.filter(section__course__user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class TopicDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Topic.objects.all()
     serializer_class = TopicSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Topic.objects.filter(section__course__user=self.request.user)
